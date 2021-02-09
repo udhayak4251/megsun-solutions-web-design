81cab5439a1315552eb6b371990ce4bea4126bf5
HelloDaily <4819674-hellodaily@users.noreply.gitlab.com>
2021-02-10 02:04:39 +0530

    first changes

---
+++ index.js
@@ -0,0 +1,39 @@
+//navigation active or inactive work
+const menuItems = document.getElementsByClassName("menu-items");
+function LinkAction() {
+    for (let index = 0; index < menuItems.length; index++) {
+        const element = menuItems[index];
+        element.classList.remove('active')
+    }
+    this.classList.add('active');
+    }
+for (let index = 0; index < menuItems.length; index++) {
+    const element = menuItems[index];
+    element.addEventListener("click", LinkAction);
+}
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+//navigation active or inactive work for mobile
+const menuItemsMobile = document.getElementsByClassName("menu-mobile-items");
+function LinkActionForMobile() {
+    for (let index = 0; index < menuItemsMobile.length; index++) {
+        const element = menuItemsMobile[index];
+        element.classList.remove('active-mobile')
+    }
+    this.classList.add('active-mobile');
+    }
+for (let index = 0; index < menuItemsMobile.length; index++) {
+    const element = menuItemsMobile[index];
+    element.addEventListener("click", LinkActionForMobile);
+}
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+    const show = document.querySelector('.menu-mobile-section');
+    show.classList.toggle('menu-mobile-section-show');
+}
